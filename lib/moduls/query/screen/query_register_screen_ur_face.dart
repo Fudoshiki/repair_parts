9961aaddc2dd2441ff758f325e5b61bda8252d00
Kollkaa <1kolia1@ukr.net>
2021-11-19 16:03:45 +0200

    blocks 1-21,25,26,35

---
+++ lib/moduls/query/screen/query_register_screen_ur_face.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:repair_parts/moduls/main/component/bottom_item.dart';
+import 'package:repair_parts/moduls/query/screen/query_register_screen_ur_face2.dart';
+
+class QueryRegisterScreenUrFace extends StatelessWidget{
+
+  bool? bottom;
+  QueryRegisterScreenUrFace({@required this.bottom});
+  BottomNavigationItem _bottomNavigationitem = BottomNavigationItem();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: bottom!=null? SizedBox(
+        height: 50,
+        child: CupertinoTabBar(
+          border: Border(
+            top: BorderSide(
+              width: 1,
+              color: Color(0xffE7E7E7),
+            ),
+          ),
+          backgroundColor: Colors.white,
+          items: [
+            _bottomNavigationitem.showItem(
+              true,
+              "home_icon.png",
+              18,
+              text: "Главная",
+            ),
+            _bottomNavigationitem.showItem(
+              false,
+              "orders_icon.png",
+              18,
+              text: "Заказы",
+            ),
+            _bottomNavigationitem.showItem(
+              false,
+              "bascket_icon.png",
+              18,
+              text: "Корзина",
+
+            ),
+            _bottomNavigationitem.showItem(
+              false,
+              "message_icon.png",
+              18,
+              text: "Диалоги",
+
+            ),
+            _bottomNavigationitem.showItem(
+              false,
+              "profile_icon.png",
+              18,
+              text: "Кабинет",
+
+            ),
+          ],
+        ),
+      ):Container(
+        height: 1,
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              margin: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 20
+              ),
+              height: 40,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    child:  Container(
+                      child: Icon(Icons.arrow_back),
+                    ),
+                    onTap: (){
+                      Get.back();
+                    },
+                  )
+                ],
+              )
+          ),
+          Container(
+              margin: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 17
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Регистрация",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 28,
+                            color: Color(0xff2e2e33),
+                            fontFamily: "Roboto"
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "ИП/Юридическое лицо",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 18,
+                            color: Color(0xff2e2e33),
+                            fontFamily: "Roboto"
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Данные о компании",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                            color: Color(0xff2e2e33),
+                            fontFamily: "Roboto"
+                        ),
+                      ),
+                      Container(
+                         margin: EdgeInsets.only(top: 8),
+                          height: 48,
+                          width: Get.width-40,
+                          padding: EdgeInsets.only(
+                            left: 20,
+                            right: 20
+                          ),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              color: Colors.white,
+                              border: Border.all(
+                                  color: Color(0xffC4C4C4),
+                                  width: 1
+                              )
+                          ),
+                          child: TextField(
+                            onTap: (){
+                            },
+                            decoration: InputDecoration(
+                              labelText: "ИНН",
+                                labelStyle:TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 12,
+                                    color: Color(0xff717171),
+                                    fontFamily: "Roboto"
+
+                                ),
+                                hintText: "1234567890",
+                                hintStyle: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 14,
+                                    color: Color(0xff959595),
+                                    fontFamily: "Roboto"
+
+                                ),
+                                enabledBorder: InputBorder.none,
+                                border: InputBorder.none,
+                                focusedErrorBorder: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                                errorBorder: InputBorder.none,
+                                disabledBorder: InputBorder.none,
+                            ),
+                          )
+                      ),
+                    ],
+                  ),
+                ),
+
+
+
+              ],
+            ),
+          ),
+          GestureDetector(
+            child: Container(
+              margin: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 40,
+                  bottom: 20
+              ),
+              decoration: BoxDecoration(
+                  color: Color(0xffE6332A),
+                  borderRadius: BorderRadius.circular(6)
+              ),
+              height: 50,
+              child: Center(
+                child: Text(
+                  "Продолжить",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                      fontFamily: "Roboto"
+                  ),
+                ),
+              ),
+            ),
+            onTap: (){
+              Get.to(QueryRegisterScreenUrFace2());
+            },
+          )
+
+        ],
+      ),
+    );
+  }
+
+}
